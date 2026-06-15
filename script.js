a61dee0de6efe377c7e96c02ed43860b37c06039
Zver-ok <tkatashev@bk.ru>
2026-06-15 16:29:22 +0500

    4

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-script.js
+const toggle = document.querySelector(".nav__toggle");
+const menu = document.querySelector(".nav__menu");
+
+if (toggle && menu) {
+  toggle.addEventListener("click", () => {
+    const isOpen = menu.classList.toggle("is-open");
+    toggle.setAttribute("aria-expanded", String(isOpen));
+  });
+
+  menu.addEventListener("click", (event) => {
+    if (event.target instanceof HTMLAnchorElement) {
+      menu.classList.remove("is-open");
+      toggle.setAttribute("aria-expanded", "false");
+    }
+  });
+}
